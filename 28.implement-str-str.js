67dc3ab8e1c069bfc34cb1c0f2e7d327b29cd2c9
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-16 15:20:57 +0100

    Finds a needle in a haystack

---
+++ 28.implement-str-str.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Implement strStr()
+ */
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  if (!needle.length) return 0;
+
+  return haystack.indexOf(needle);
+};
+
