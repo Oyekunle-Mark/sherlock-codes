60c8fa7823b899b372cdf37d92b67b0f2388e646
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-17 14:26:50 +0100

    Performs basic operations with *, / and %

--- operator.js
+++ operator.js
@@ -1,3 +1,9 @@
+/**
+ * Performs basic operations without *, / and %
+ * @param {*} num1
+ * @param {*} num2
+ * @returns
+ */
 function negCheck (num1, num2) {
   if (num1 < 0 && num2 < 0 || num1 > 0 && num2 > 0) 
     return [false, Math.abs(num1), Math.abs(num2)];
