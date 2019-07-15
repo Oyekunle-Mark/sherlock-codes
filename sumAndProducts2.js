cc2108756ca6f54e936a69cf9953b3b0f777d634
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-15 11:28:43 +0100

    Second solution of the sum and product problem

---
+++ sumAndProducts2.js
@@ -0,0 +1,26 @@
+/**
+ * Returns the numbers that adds up to the first argument and equals the second argument when multiplied
+ * The result is always an array sorted by number
+ * @param {number} sum
+ * @param {number} product
+ * @returns {number[]}
+ */
+function sumAndProduct(sum, product) {
+  let start = 0, end = sum;
+  let validCombo = false;
+  
+  for(start; start <= end; start++) {
+    if ((start + end === sum) && (start * end === product)) {
+      validCombo = true;
+      break;
+    }
+    
+    end--;
+  }
+  
+  return validCombo ? [start, end].sort((a, b) => a > b) : null;
+}
+
+console.log(sumAndProduct(6, 9));
+console.log(sumAndProduct(20, 0));
+console.log(sumAndProduct(36, 180));
