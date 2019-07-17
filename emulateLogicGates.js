583846f7ed5c1b64cd734ea4ce53bcd61448cc94
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-17 08:12:01 +0100

    Documented the functions

--- emulateLogicGates.js
+++ emulateLogicGates.js
@@ -1,21 +1,44 @@
+/**
+ * *Returns the not of a value
+ * @param {any} b
+ * @returns {boolean}
+ */
 function NOT(b) {
   if (b) return false;
 
   return true;
 }
 
+/**
+ * *Returns the and of two arguments
+ * @param {any} x
+ * @param {any} y
+ * @returns {boolean}
+ */
 function AND(x, y) {
   if (x) if (y) return true;
 
   return false;
 }
 
+/**
+ * *Returns the not and of two arguments
+ * @param {any} x
+ * @param {any} y
+ * @returns {boolean}
+ */
 function NAND(x, y) {
   if (x) if (y) return false;
 
   return true;
 }
 
+/**
+ * *Returns the inclusive or of two arguments
+ * @param {any} x
+ * @param {any} y
+ * @returns {boolean}
+ */
 function OR(x, y) {
   if (x) return true;
 
@@ -24,6 +47,12 @@ function OR(x, y) {
   return false;
 }
 
+/**
+ * *Returns the exclusive or of two arguments
+ * @param {any} x
+ * @param {any} y
+ * @returns {boolean}
+ */
 function XOR(x, y) {
   if (x) if (y) return false;
 
