8474b194e3465fffad043ba980a83165873bcc1e
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-01 10:43:46 +0100

    The birthday cake candle hacker rank challenge

---
+++ birthdayCakeCandles.js
@@ -0,0 +1,19 @@
+function birthdayCakeCandles(ar) {
+  const max = Math.max(...ar);
+  let count = 0;
+
+  [...ar].map(num => {
+    if (num === max) count++;
+  });
+
+  return count;
+}
+
+console.log(
+  'TCL: birthdayCakeCandles([3, 2, 1, 3])',
+  birthdayCakeCandles([3, 2, 1, 3]),
+);
+console.log(
+  'TCL: birthdayCakeCandles([82, 49, 82, 82, 41, 82, 15, 63, 38, 25])',
+  birthdayCakeCandles([82, 49, 82, 82, 41, 82, 15, 63, 38, 25]),
+);
