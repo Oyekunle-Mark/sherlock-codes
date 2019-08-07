5b3ac074c29e5d4cd10e7dc6ab78057eb6803a18
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-07 10:09:53 +0100

    Hacker rank jumping clouds challenge

---
+++ jumpingOnClouds.js
@@ -0,0 +1,28 @@
+function jumpingOnClouds(c) {
+  let jumps = 0;
+
+  for (let i = 0; i < c.length; i++) {
+    if (c[i] === c[i + 1] && c[i] === c[i + 2]) {
+      i++;
+      jumps++;
+      continue;
+    }
+  
+    if (c[i + 1] === 0) jumps++;
+  }
+
+  return jumps;
+}
+
+console.log(
+  'TCL: jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])',
+  jumpingOnClouds([0, 0, 1, 0, 0, 1, 0]),
+);
+console.log(
+  'TCL: jumpingOnClouds([0, 0, 0, 0, 1, 0])',
+  jumpingOnClouds([0, 0, 0, 0, 1, 0]),
+);
+console.log(
+  'TCL: jumpingOnClouds([0, 0, 0, 1, 0, 0])',
+  jumpingOnClouds([0, 0, 0, 1, 0, 0]),
+);
