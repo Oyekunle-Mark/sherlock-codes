afd8977caa293a56b54d5a4c99ac4e4ea98e59a2
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-09 14:40:47 +0100

    Hacker rank migratory bird challenge

---
+++ migratoryBirds.js
@@ -0,0 +1,28 @@
+function migratoryBirds(arr) {
+  const findMostFrequencyForNumberOneToFive = ar => {
+    const mostFrequent = [];
+
+    const range = new Map([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0]]);
+
+    ar.map(num => range.set(num, range.get(num) + 1));
+
+    const maximum = Math.max(...range.values());
+
+    range.forEach((val, key) => {
+      if (val === maximum) mostFrequent.push(key);
+    });
+
+    return mostFrequent;
+  };
+
+  return Math.min(...findMostFrequencyForNumberOneToFive(arr));
+}
+
+console.log(
+  'TCL: migratoryBirds([1, 4, 4, 4, 5, 3])',
+  migratoryBirds([1, 4, 4, 4, 5, 3]),
+);
+console.log(
+  'TCL: migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4])',
+  migratoryBirds([1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]),
+);
