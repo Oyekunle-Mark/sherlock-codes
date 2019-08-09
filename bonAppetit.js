f7d31225346715e556c80363367bd3bd724030c5
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-09 14:40:26 +0100

    Hacker rank bon appetiti challenge

---
+++ bonAppetit.js
@@ -0,0 +1,12 @@
+function bonAppetit(bill, k, b) {
+  const billWithoutAnna = [...bill];
+  billWithoutAnna.splice(k, 1);
+
+  const annaBill = billWithoutAnna.reduce((acc, val) => acc + val, 0) / 2;
+
+  if (annaBill === b) console.log('Bon Appetit');
+  else console.log(b - annaBill);
+}
+
+bonAppetit([3, 10, 2, 9], 1, 12);
+bonAppetit([3, 10, 2, 9], 1, 7);
