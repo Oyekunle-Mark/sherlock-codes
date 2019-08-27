f2f7e2466d167f47073340e10f6d61578a10aa8e
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-27 10:11:53 +0100

    Hacker rank day of the programmer challenge

---
+++ dayOfTheProgrammer.js
@@ -0,0 +1,18 @@
+function dayOfProgrammer(year) {
+  if (year >= 1700 && year <= 1917) {
+    if (year % 4 === 0) return `12.09.${year}`;
+  }
+
+  if (year >= 1919) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)
+      return `12.09.${year}`;
+  }
+
+  if (year === 1918) return `26.09.${year}`;
+
+  return `13.09.${year}`;
+}
+
+console.log(dayOfProgrammer(2016));
+console.log(dayOfProgrammer(2017));
+console.log(dayOfProgrammer(1800));
