a22378256a2120148b0f159d660a6d9119db0130
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-10-14 15:33:09 +0100

    commented the algorithm

--- balancedBracket.py
+++ balancedBracket.py
@@ -1,4 +1,8 @@
 class Stack:
+    """Implementation of simple stack data structure
+    with the methods push, pop and length
+    """
+
     def __init__(self):
         self.storage = []
 
@@ -6,6 +10,7 @@ class Stack:
         self.storage.append(value)
 
     def pop(self):
+        # if stack is empty return None
         if not self.storage:
             return None
 
@@ -16,24 +21,34 @@ class Stack:
 
 
 def isBalanced(s):
+    # will allow access to the correct opening bracket
     mapBrackets = {
         ")": "(",
         "]": "[",
         "}": "{"
     }
 
+    # initiate the stack data storage
     stack = Stack()
 
+    # loop through the input string
     for bracket in s:
+        # if it is an opening bracket
         if bracket in "({[":
+            # push it to the stack
             stack.push(bracket)
         else:
+            # check if the closing closes the correct opening tag
+            # return NO if it does not
             if stack.pop() is not mapBrackets[bracket]:
                 return "NO"
 
+    # after popping items and the stack is still not empty
+    # then the brackets is not balanced
     if stack.length():
         return "NO"
 
+    # if stack is empty then brackets are balanced
     return "YES"
 
 
