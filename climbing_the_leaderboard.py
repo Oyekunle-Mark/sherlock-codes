d0a73036255a2dce2a1ba50cfcc460da4ced37fb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-18 09:55:23 +0100

    Climbing the leaderboard skeleton

---
+++ climbing_the_leaderboard.py
@@ -0,0 +1,8 @@
+def climbingLeaderboard(scores, alice):
+    """ It should return an integer array where each element represents Alice's rank after the game.
+
+    Arguments:
+        scores {list} -- an array of integers that represent leaderboard scores 
+        alice {list} -- an array of integers that represent Alice's scores 
+    """
+    pass
