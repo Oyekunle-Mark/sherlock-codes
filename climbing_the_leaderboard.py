27f86ae08ccbe8107b87c152a082d7e84c6ffdac
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-18 20:03:26 +0100

    First pass solutioin

--- climbing_the_leaderboard.py
+++ climbing_the_leaderboard.py
@@ -16,22 +16,36 @@ def climbingLeaderboard(scores, alice):
     # loop  while current_pos is less than length of scores
     while current_pos < len(scores):
         # if item at current_score_pos of alice is greater than item at current_pos of scores
+        if alice[current_score_pos] > scores[current_pos]:
             # append pos to alice_pos
+            alice_pos.append(pos)
             # increment pos
+            pos += 1
             # decrement current_score_pos
+            current_score_pos -= 1
         # otherwise if item at current_score_pos of alice is equal to item at current_pos of scores
+        elif alice[current_score_pos] == scores[current_pos]:
             # append pos to alice_pos
+            alice_pos.append(pos)
             # increment pos
+            pos += 1
             # decrement current_score_pos
+            current_score_pos -= 1
             # increment current_pos
+            current_pos += 1
         # otherwise
+        else:
             # increment current_pos
+            current_pos += 1
 
     # loop while current_score_pos is >= 0
+    while current_score_pos is >= 0:
         # append pos to alice_pos
+        alice_pos.append(pos)
         # increment pos
+        pos += 1
         # decrement current_score_pos
+        current_score_pos -= 1
 
     # return the result of reversing alice_pos
-    pass
-
+    return alice_pos
