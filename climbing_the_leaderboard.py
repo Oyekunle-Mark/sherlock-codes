473dd04f00333eaaf1f612e6da64000c42b794b0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-19 08:52:56 +0100

    New skeleton for the problem

--- climbing_the_leaderboard.py
+++ climbing_the_leaderboard.py
@@ -1,3 +1,7 @@
+def find_score_pos(scores, s):
+    pass
+
+
 def climbingLeaderboard(scores, alice):
     """ It should return an integer array where each element represents Alice's rank after the game.
 
@@ -5,62 +9,6 @@ def climbingLeaderboard(scores, alice):
         scores {list} -- an array of integers that represent leaderboard scores 
         alice {list} -- an array of integers that represent Alice's scores 
     """
-    # initialize  pos to one
-    pos = 0
-    # initialize current_pos to zero
-    current_pos = 0
-    # initialize current_score_pos to length of alice - 1
-    current_score_pos = len(alice) - 1
-    # initialize alice_pos to empty list
-    alice_pos = []
-    # loop  while current_pos is less than length of scores
-    item = scores[0]
-    while current_pos < len(scores):
-        # if item at current_score_pos of alice is greater than item at current_pos of scores
-        if alice[current_score_pos] > scores[current_pos]:
-            # increment pos
-            pos += 1
-            # append pos to alice_pos
-            alice_pos.append(pos)
-            # decrement current_score_pos
-            current_score_pos -= 1
-        # otherwise if item at current_score_pos of alice is equal to item at current_pos of scores
-        elif alice[current_score_pos] == scores[current_pos]:
-            if item > scores[current_pos]:
-                # increment pos
-                pos += 1
-            # append pos to alice_pos
-            alice_pos.append(pos)
-            # decrement current_score_pos
-            current_score_pos -= 1
-            # increment current_pos
-            current_pos += 1
-        # otherwise
-        else:
-            if item > scores[current_pos]:
-                # increment pos
-                pos += 1
-            # increment current_pos
-            current_pos += 1
-
-        # if current_pos:
-        item = scores[current_pos - 1]
-
-    print(alice_pos, current_score_pos, pos)
-
-    # loop while current_score_pos is >= 0
-    while current_score_pos >= 0:
-        # append pos to alice_pos
-        alice_pos.append(pos)
-        # increment pos
-        pos += 1
-        # decrement current_score_pos
-        current_score_pos -= 1
-
-    # return the result of reversing alice_pos
-    return alice_pos
-
-def climbingLeaderboards(scores, alice):
     pass
 
 
