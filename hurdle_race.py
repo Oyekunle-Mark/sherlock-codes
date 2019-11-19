f1b10286219485a96b47173f941907ba2c342b67
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-19 18:24:23 +0100

    First pass solution

--- hurdle_race.py
+++ hurdle_race.py
@@ -1,7 +1,11 @@
 def hurdleRace(k, height):
     # Find and save the maximum value in height in max_height
+    max_height = max(height)
     # if k is greater than or equal max_height
+    if k >= max_height:
         # return 0
+        return 0
     # otherwise
+    else:
         # return max_height - k
-    pass
+        return max_height - k
