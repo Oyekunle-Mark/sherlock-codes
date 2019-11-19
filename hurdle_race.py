630c0334cf1ec075c9f2ab274cb98ca896632989
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-19 17:23:02 +0100

    Hurdle race psudocode

--- hurdle_race.py
+++ hurdle_race.py
@@ -1,2 +1,7 @@
 def hurdleRace(k, height):
+    # Find and save the maximum value in height in max_height
+    # if k is greater than or equal max_height
+        # return 0
+    # otherwise
+        # return max_height - k
     pass
