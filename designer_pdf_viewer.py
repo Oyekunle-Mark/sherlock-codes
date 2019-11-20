9b52f4f5d85e9a578fd2827938c01ce7145f4368
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-20 12:44:03 +0100

    designer pdf viewer pseudocode

--- designer_pdf_viewer.py
+++ designer_pdf_viewer.py
@@ -1,2 +1,13 @@
 def designerPdfViewer(h, word):
+    # initialize letters to the letters in the English alphabet
+    # initialize letters_to_height to empty dictionary
+    # loop through a zip of h and letters
+        # set the height of each letters
+    # set length to zero
+    # set max_height to one
+    # loop through word
+        # if letter is greater than max_height
+            # set max_height to letter
+        # increment length
+    # return the product of length and max_height
     pass
