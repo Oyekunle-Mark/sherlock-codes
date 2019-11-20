89b0406c918196dcda785ed650e8267813019f69
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-20 12:48:34 +0100

    First pass solution

--- designer_pdf_viewer.py
+++ designer_pdf_viewer.py
@@ -1,13 +1,24 @@
 def designerPdfViewer(h, word):
     # initialize letters to the letters in the English alphabet
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+               "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     # initialize letters_to_height to empty dictionary
+    letters_to_height = {}
     # loop through a zip of h and letters
+    for letter, height in zip(letters, h):
         # set the height of each letters
+        letters[letter] = height
     # set length to zero
+    length = 0
     # set max_height to one
+    max_height = 1
     # loop through word
+    for letter in word:
         # if letter is greater than max_height
+        if letters[letter] > max_height:
             # set max_height to letter
+            max_height = letters[letter]
         # increment length
+        length += 1
     # return the product of length and max_height
-    pass
+    return length * max_height
