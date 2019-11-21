8879a7ea7f1d70cc78bd8cfc40e32d35f9f2fa14
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-21 09:52:12 +0100

    Utopian tree first pass solution

--- utopian_tree.py
+++ utopian_tree.py
@@ -1,11 +1,19 @@
 def utopianTree(n):
     # set cycle to zero
+    cycle = 0
     # set height to 1
+    height = 1
     # loop while cycle is less than or equals n
+    while cycle <= n:
         # increment cycle by one
+        cycle += 1
         # check if cycle is odd
+        if cycle % 2 == 1:
             # double height
+            height *= 2
         # otherwise
+        else:
             # increment height by one
+            height += 1
     # return height
-    pass
+    return height
