7e8fb7d3c17dae4ab87d95fe30c6ab7150ce6b24
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-21 09:50:19 +0100

    Utopian tree psudocode

--- utopian_tree.py
+++ utopian_tree.py
@@ -1,2 +1,11 @@
 def utopianTree(n):
+    # set cycle to zero
+    # set height to 1
+    # loop while cycle is less than or equals n
+        # increment cycle by one
+        # check if cycle is odd
+            # double height
+        # otherwise
+            # increment height by one
+    # return height
     pass
