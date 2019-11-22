b277832cf94898b39952a7ef9604a5a489fcc0d5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 11:23:06 +0100

    Increment count

--- distinct_pairs.py
+++ distinct_pairs.py
@@ -1,2 +1,7 @@
 def countPairs(arr, k):
+    # set the count to zero
+    # loop through arr
+        # loop from current number to the end
+            # if it sums to k
+                # increment count
     pass
