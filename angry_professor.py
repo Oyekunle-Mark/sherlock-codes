a0b07706ae79bec1e8c9f7745cfea66e433a5454
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 12:12:13 +0100

    Pseudocode for angry professor problem

--- angry_professor.py
+++ angry_professor.py
@@ -1,2 +1,9 @@
 def angryProfessor(k, a):
+    # set count to zero
+    # loop through a
+        # if time is less than or equal 0
+            # increment count
+    # if count is greater than or or equals k
+        # return NO
+    # return YES
     pass
