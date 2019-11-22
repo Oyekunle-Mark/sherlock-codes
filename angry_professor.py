12e269596acbc25ffd1a90fe5fe654ea37ff8a2d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 12:22:18 +0100

    Added free lines

--- angry_professor.py
+++ angry_professor.py
@@ -1,16 +1,19 @@
 def angryProfessor(k, a):
     # set count to zero
     count = 0
+
     # loop through a
     for time in a:
         # if time is less than or equal 0
         if time <= 0:
             # increment count
             count += 1
+
     # if count is greater than or or equals k
     if count >= k:
         # return NO
         return "NO"
+
     # return YES
     return "YES"
 
