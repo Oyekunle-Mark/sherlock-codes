c5ac2cac1b68dd034e9f388fa82013f183f02bb8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 11:38:24 +0100

    First pass solution

--- distinct_pairs.py
+++ distinct_pairs.py
@@ -1,7 +1,7 @@
 def countPairs(arr, k):
     # set the count to zero
     count = 0
-    # set occurences to empty dict
+    # set occurrences to empty dict
     occ = {}
     # loop through arr
     for i in range(len(arr)):
@@ -10,14 +10,21 @@ def countPairs(arr, k):
             # if it sums to k
             if arr[i] + arr[j] == k:
                 # if arr[i] in occ or arr[j] in occ
+                if arr[i] in occ or arr[j] in occ:
                     # if arr[i] of occ is arr[j]
+                    if occ[arr[i]] == arr[j]:
                         # continue
+                        continue
                     # elif arr[j] of occ is arr[i]
+                    elif occ[arr[j]] == arr[i]:
                         # continue
+                        continue
                 # increment count
                 count += 1
-                #set arr[i] and arr[j] in occ
-                
+                # set arr[i] and arr[j] in occ
+                occ[arr[i]] = arr[j]
+                occ[arr[j]] = arr[i]
+
     # return count
     return count
 
