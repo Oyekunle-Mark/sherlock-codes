3bd1fc0ed0ac7d7525cba64db1aa8371d9607616
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 10:37:42 +0100

    Pseudocode

--- miminum_unique_array_square.py
+++ miminum_unique_array_square.py
@@ -1,2 +1,10 @@
 def getMinimumUniqueSum(arr):
+    # create a set unique_numbers
+    # loop through arr
+        # if number is not in unique_numbers
+            # add it
+        # otherwise
+            # loop while number in unique_numbers
+                # increment number
+    # return sum of unique_numbers
     pass
