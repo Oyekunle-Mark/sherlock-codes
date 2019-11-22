99ec87858b3d01924b9da058535cad1ec5c0c052
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 10:39:29 +0100

    First pass solution

--- miminum_unique_array_square.py
+++ miminum_unique_array_square.py
@@ -1,10 +1,17 @@
 def getMinimumUniqueSum(arr):
     # create a set unique_numbers
+    unique_numbers = set()
     # loop through arr
+    for number in arr:
         # if number is not in unique_numbers
+        if number not in unique_numbers:
             # add it
+            unique_numbers.add(number)
         # otherwise
+        else:
             # loop while number in unique_numbers
+            while number in unique_numbers:
                 # increment number
+                number += 1
     # return sum of unique_numbers
-    pass
+    return sum(unique_numbers)
