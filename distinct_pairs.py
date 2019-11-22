6113c7a40f6682acae22a3f330e763ddac214a5e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 11:25:41 +0100

    First pass solution

--- distinct_pairs.py
+++ distinct_pairs.py
@@ -1,7 +1,13 @@
 def countPairs(arr, k):
     # set the count to zero
+    count = 0
     # loop through arr
+    for i in range(len(arr)):
         # loop from current number to the end
+        for j in range(i, len(arr)):
             # if it sums to k
+            if arr[i] + arr[j] == k:
                 # increment count
-    pass
+                count += 1
+    # return count
+    return count
