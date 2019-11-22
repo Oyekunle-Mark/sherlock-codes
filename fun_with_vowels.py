243a8f5f41b2e332253827b68d92c316550b0667
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-22 10:55:28 +0100

    Pseudocode

--- fun_with_vowels.py
+++ fun_with_vowels.py
@@ -1,2 +1,19 @@
 def longestVowelSubsequence(s):
+    # create vowel with aeiou
+    # set pos to zero
+    # set length to zero
+    # loop through s
+        # if pos equals four
+            # if char is char at pos of vowel
+                # increment length
+                # continue
+        # if char is char at pos of vowel
+            # increment length
+        # otherwise if char is char at pos + 1 of vowel
+            # increment length
+            # increment pos
+    # if pos equals four
+        # return length
+    # otherwise
+        # return 0
     pass
