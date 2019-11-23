b3788db354e6fdc5a5c0e5c66e0480aad259b1ab
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-23 08:43:53 +0100

    Added free lines

--- beautiful_day_at_the_movies.py
+++ beautiful_day_at_the_movies.py
@@ -3,6 +3,7 @@ def reverseInts(n):
     str_n = str(n)
     # reverse the string
     reversed_n = str_n[::-1]
+
     # return the int representation
     return int(reversed_n)
 
@@ -10,18 +11,22 @@ def reverseInts(n):
 def beautifulDays(i, j, k):
     # set count to zero
     count = 0
+
     # loop while i is less than or equal j
     while i <= j:
         # reverse number
         reversed_number = reverseInts(i)
         # set the absolute of number minus reverse_number to diff
         diff = abs(i - reversed_number)
+
         # if diff modulo k is zero
         if diff % k == 0:
             # increment count
             count += 1
+
         # increment i
         i += 1
+
     # return count
     return count
 
