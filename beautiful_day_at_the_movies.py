ecbaacf1560c132ed01fd8ceb20caf1b37fa0970
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-23 08:31:33 +0100

    Added pseudocode

--- beautiful_day_at_the_movies.py
+++ beautiful_day_at_the_movies.py
@@ -1,6 +1,16 @@
 def reverseInts(n):
+    # convert the integer to string
+    # reverse the string
+    # return the int representation
     pass
 
 
 def beautifulDays(i, j, k):
+    # set count to zero
+    # loop while i is less than or equal j
+        # reverse number
+        # set the absolute of number minus reverse_number to diff
+        # if diff modulo k is zero
+            # increment count
+    # return count
     pass
