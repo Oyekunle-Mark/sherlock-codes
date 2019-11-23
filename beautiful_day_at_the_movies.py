b2d6111f83d1e99427803e1447dec5a2a7acd882
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-23 08:35:53 +0100

    First pass solution

--- beautiful_day_at_the_movies.py
+++ beautiful_day_at_the_movies.py
@@ -1,16 +1,26 @@
 def reverseInts(n):
     # convert the integer to string
+    str_n = str(n)
     # reverse the string
+    reversed_n = str_n[::-1]
     # return the int representation
-    pass
+    return int(reverseInts)
 
 
 def beautifulDays(i, j, k):
     # set count to zero
+    count = 0
     # loop while i is less than or equal j
+    while i <= j:
         # reverse number
+        reversed_number = reverseInts(i)
         # set the absolute of number minus reverse_number to diff
+        diff = abs(i - reversed_number)
         # if diff modulo k is zero
+        if diff % k == 0:
             # increment count
+            count++
+        # increment i
+        i += 1
     # return count
-    pass
+    return count
