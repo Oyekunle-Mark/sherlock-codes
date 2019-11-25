af1b9860f34985f7c96e80960ecb49e88d81a17b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 08:50:28 +0100

    Pseudocode for a first pass solution

--- save_the_prisoner.py
+++ save_the_prisoner.py
@@ -1,2 +1,11 @@
 def saveThePrisoner(n, m, s):
+    # set a variable start to one
+    # set a variable chair to s
+    # loop while start is less than m minus one
+        # check if chair equals n
+            # set chair to one
+            # continue
+        # increment chair
+        # increment start
+    # return chair
     pass
