15334b7eac0d3b69e98c93813be056a30cbc8af2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 08:32:22 +0100

    First apss solution

--- viral_advertising.py
+++ viral_advertising.py
@@ -1,9 +1,15 @@
 def viralAdvertising(n):
     # initialize a variable liked to zero
+    liked = 0
     # initialize a variable shared to five
+    shared = 5
     # initialize a variable days to zero
+    days = 0
     # loop while days is less than n
+    while days < n:
         # increment liked by the floor division of shared and 2
-        # set shared to liked times three
+        liked += shared % 2
+        # set shared to floor division of shared and 2 times three
+        shared = (shared % 2) * 3
     # return liked
-    pass
+    return liked
