54ebf7515975a3d77354e968f031bc3b29679530
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 08:29:48 +0100

    Pseudocode

--- viral_advertising.py
+++ viral_advertising.py
@@ -1,2 +1,9 @@
 def viralAdvertising(n):
+    # initialize a variable liked to zero
+    # initialize a variable shared to five
+    # initialize a variable days to zero
+    # loop while days is less than n
+        # increment liked by the floor division of shared and 2
+        # set shared to liked times three
+    # return liked
     pass
