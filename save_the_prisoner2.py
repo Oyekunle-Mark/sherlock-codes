756c03ea825f30e9e46a05b6ec74694dd2c93eb0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 09:28:49 +0100

    Second pass solution no loop

--- save_the_prisoner2.py
+++ save_the_prisoner2.py
@@ -1,12 +1,18 @@
 def saveThePrisoner(n, m, s):
     # check if s plus m is less than n
+    if s + m < n:
         # return s plus m minus one
+        return s + m - 1
     # else
+    else:
         # check if modulo of s plus m minus one and n is zero
+        if (s + m - 1) % n == 0:
             # return s
+            return s
         # otherwise
+        else:
             # return modulo of s plus m minus one and n
-    pass
+            return (s + m - 1) % n
 
 
 print(saveThePrisoner(5, 2, 1))  # 2
