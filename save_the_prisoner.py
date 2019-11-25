25422f0615509c8214dff9814c164a6c845aea79
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 08:52:00 +0100

    Implementation of a first pass solution

--- save_the_prisoner.py
+++ save_the_prisoner.py
@@ -1,11 +1,19 @@
 def saveThePrisoner(n, m, s):
     # set a variable start to one
+    start = 0
     # set a variable chair to s
+    chair = s
     # loop while start is less than m minus one
+    while start < m - 1:
         # check if chair equals n
+        if chair == n:
             # set chair to one
+            chair = 1
             # continue
+            continue
         # increment chair
+        chair += 1
         # increment start
+        start += 1
     # return chair
-    pass
+    return chair
