ff6b42efa1ba8d80e7d50e2312fd29f9b00ede0e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-25 09:00:24 +0100

    Fixed bug and added tests

--- save_the_prisoner.py
+++ save_the_prisoner.py
@@ -9,6 +9,8 @@ def saveThePrisoner(n, m, s):
         if chair == n:
             # set chair to one
             chair = 1
+            # increment start
+            start += 1
             # continue
             continue
         # increment chair
@@ -17,3 +19,9 @@ def saveThePrisoner(n, m, s):
         start += 1
     # return chair
     return chair
+
+
+print(saveThePrisoner(5, 2, 1))  # 2
+print(saveThePrisoner(5, 2, 2))  # 3
+print(saveThePrisoner(7, 19, 2))  # 6
+print(saveThePrisoner(3, 7, 3))  # 3
