648a28d6d73e164b275d84f3caa1c93e21f9198f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-26 09:31:47 +0100

    sequence equaltion pseudocode

--- sequence_equation.py
+++ sequence_equation.py
@@ -1,2 +1,12 @@
 def permutationEquation(p):
+    # set length to length of p
+    # set start to 1
+    # initialize y to empty list
+    # initialize indices to empty dictionary
+    # loop through p
+        # add the current number to indices in manner number, index + 1
+    # loop while start is less than or equals length
+        # find index of start from indices as set to index
+        # find the index of index itself and append to y
+    # return y
     pass
