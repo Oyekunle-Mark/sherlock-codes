b697d8159b7e5a872eac5b0de3b970560fafe8fa
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-26 08:52:52 +0100

    Circular array rotation pseudocode

--- circular_array_rotation.py
+++ circular_array_rotation.py
@@ -2,4 +2,15 @@ from collections import deque
 
 
 def circularArrayRotation(a, k, queries):
+    # create a deque instance with a
+    # initialize rotation_count to zero
+    # loop while rotation_count is less than or equals k
+        # pop from the deque
+        # appendLeft the item popped
+        # increment rotation_count
+    # set length to the length of queries
+    # create an array of length length
+    # loop through queries
+        # place item at index to new array
+    # return new array
     pass
