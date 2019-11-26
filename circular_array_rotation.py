432dac52ccc5cddbc16e92157c01773ad33f80c5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-26 08:57:40 +0100

    First pass solution

--- circular_array_rotation.py
+++ circular_array_rotation.py
@@ -3,14 +3,24 @@ from collections import deque
 
 def circularArrayRotation(a, k, queries):
     # create a deque instance with a
+    d = deque(a)
     # initialize rotation_count to zero
-    # loop while rotation_count is less than or equals k
+    rotation_count = 0
+    # loop while rotation_count is less than k
+    while rotation_count < k:
         # pop from the deque
+        item = d.pop()
         # appendLeft the item popped
+        d.appendleft(item)
         # increment rotation_count
+        rotation_count += 1
     # set length to the length of queries
+    length = len(queries)
     # create an array of length length
+    indices = [None] * length
     # loop through queries
+    for i, n in enumerate(queries):
         # place item at index to new array
+        indices[i] = d[n]
     # return new array
-    pass
+    return indices
