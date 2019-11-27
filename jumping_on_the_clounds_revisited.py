a87421fb61a684c3c144d6296550b2ca918b497c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 09:32:24 +0100

    Jumping on the clouds pseudocode

--- jumping_on_the_clounds_revisited.py
+++ jumping_on_the_clounds_revisited.py
@@ -1,2 +1,13 @@
 def jumpingOnClouds(c, k):
+    # initialize index to zero
+    # initialize length to the length of c
+    # initialize energy to 100
+    # while True
+        # index is index plus k modulo length
+        # decrement energy by one
+        # if item at the index of c is 1,
+            # decrement energy by a further two
+        # if index equals 0
+            # break
+    # return energy
     pass
