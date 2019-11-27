3677f0d12c4ac6e16665230807f43a7cc16d3f3e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-27 09:34:24 +0100

    Implemented the pseudocode

--- jumping_on_the_clounds_revisited.py
+++ jumping_on_the_clounds_revisited.py
@@ -1,13 +1,23 @@
 def jumpingOnClouds(c, k):
     # initialize index to zero
+    index = 0
     # initialize length to the length of c
+    length = len(c)
     # initialize energy to 100
+    energy = 100
     # while True
+    while True:
         # index is index plus k modulo length
+        index = (index + k) % length
         # decrement energy by one
+        energy -= 1
         # if item at the index of c is 1,
+        if c[index] == 1:
             # decrement energy by a further two
+            energy -= 2
         # if index equals 0
+        if index == 0:
             # break
+            break
     # return energy
-    pass
+    return energy
