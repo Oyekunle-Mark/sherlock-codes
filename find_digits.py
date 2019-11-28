3533bea6baceba6c80a31bf2afa4eb40f2caceb0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-28 09:08:12 +0100

    Formatted the first pass

--- find_digits.py
+++ find_digits.py
@@ -3,18 +3,21 @@ def findDigits(n):
     inp = str(n)
     # initialize count to zero
     count = 0
+
     # loop through inp
     for c in inp:
         # if character is zero
         if c == '0':
             # continue
             continue
+
         # convert the character to an int
         i = int(c)
         # if n modulo character is 0
         if n % i == 0:
             # increment count
             count += 1
+
     # return count
     return count
 
