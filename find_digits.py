450628cc3aa5bf16adf78d524a30dc23f227ee83
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-28 09:05:41 +0100

    Implemented a first pass solution

--- find_digits.py
+++ find_digits.py
@@ -1,11 +1,19 @@
 def findDigits(n):
     # convert n to a string and save in inp
+    inp = str(n)
     # initialize count to zero
+    count = 0
     # loop through inp
+    for c in inp:
         # if character is zero
+        if c == '0':
             # continue
+            continue
         # convert the character to an int
+        i = int(c)
         # if n modulo character is 0
+        if n % i == 0:
             # increment count
+            count += 1
     # return count
-    pass
+    return count
