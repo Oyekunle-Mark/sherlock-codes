4f58edeabc74a477beae87085de5e762bea08173
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-28 09:04:07 +0100

    Find digits pseudocode

--- find_digits.py
+++ find_digits.py
@@ -1,2 +1,11 @@
 def findDigits(n):
+    # convert n to a string and save in inp
+    # initialize count to zero
+    # loop through inp
+        # if character is zero
+            # continue
+        # convert the character to an int
+        # if n modulo character is 0
+            # increment count
+    # return count
     pass
