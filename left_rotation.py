d74243eb9ed544b9d9716b8415a01d689544acbc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-30 09:36:06 +0100

    Rot left pseudocode

--- left_rotation.py
+++ left_rotation.py
@@ -2,4 +2,10 @@ from collections import deque
 
 
 def rotLeft(a, d):
+    # create a deque d from list a
+    # initialize num to zero
+    # loop while num is less than d
+        # pop left from the queue and set to variable popped
+        # append the popped to the deque d
+    # return the deque
     pass
