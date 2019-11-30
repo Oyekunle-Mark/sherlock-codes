beb41660393d43db9197036d2e4f819626af5d0b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-30 09:37:32 +0100

    Implemented a first pass

--- left_rotation.py
+++ left_rotation.py
@@ -3,9 +3,16 @@ from collections import deque
 
 def rotLeft(a, d):
     # create a deque d from list a
+    d = deque(a)
     # initialize num to zero
+    num = 0
     # loop while num is less than d
+    while num < d:
         # pop left from the queue and set to variable popped
+        popped = d.popleft()
         # append the popped to the deque d
+        d.append(popped)
+        # increment num
+        num += 1
     # return the deque
-    pass
+    return d
