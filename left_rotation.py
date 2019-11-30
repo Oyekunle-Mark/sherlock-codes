adf96d8dddbf8437c295a9c61a5afd3a8ced2a41
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-30 09:39:02 +0100

    Fixed a naming related bug

--- left_rotation.py
+++ left_rotation.py
@@ -3,16 +3,19 @@ from collections import deque
 
 def rotLeft(a, d):
     # create a deque d from list a
-    d = deque(a)
+    deq = deque(a)
     # initialize num to zero
     num = 0
     # loop while num is less than d
     while num < d:
         # pop left from the queue and set to variable popped
-        popped = d.popleft()
+        popped = deq.popleft()
         # append the popped to the deque d
-        d.append(popped)
+        deq.append(popped)
         # increment num
         num += 1
     # return the deque
-    return d
+    return deq
+
+
+print(rotLeft([1, 2, 3, 4, 5], 4))
