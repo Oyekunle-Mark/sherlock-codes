9f2c090cdef1e513e1c61f9a30239530b857a629
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-02 11:30:12 +0100

    Began the second pass solution

---
+++ minimum_swaps_second_pass.py
@@ -0,0 +1,8 @@
+def minimumSwaps(arr):
+    pass
+
+
+print(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))  # 5
+print(minimumSwaps([4, 3, 1, 2]))  # 3
+print(minimumSwaps([2, 3, 4, 1, 5]))  # 3
+print(minimumSwaps([1, 3, 5, 2, 4, 6, 7]))  # 3
