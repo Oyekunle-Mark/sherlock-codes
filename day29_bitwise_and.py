4c02c4993b7dc2fcbfd94f0d6722a9cfd9d43fb7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-02 17:51:53 +0100

    Implemented the find combo algo

--- day29_bitwise_and.py
+++ day29_bitwise_and.py
@@ -1,6 +1,18 @@
 def find_combinations(n):
-    pass
+    # initialize a combo to empty dict
+    combo = {}
+    # loop from a range of one to n
+    for i in range(1, n):
+        # add the number to dict and set the value to empty set
+        combo[i] = set()
+        # loop from current number plus one to n
+        for j in range(i, n + 1):
+            # add this number to set at index i of combo
+            combo[i].add(j)
+    # return combo
+    return combo
 
 
 def bit_and(n, k):
-    pass
+    # return possible combo
+    return find_combinations(n)
