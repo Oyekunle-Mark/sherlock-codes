33a3d022001d8f14a352319d041b760612e6398d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-02 10:44:57 +0100

    Selection sort implementation

--- minimum_swaps.py
+++ minimum_swaps.py
@@ -1,4 +1,11 @@
 def minimumSwaps(arr):
     # Which sorting algorithm takes minimum number of swaps?
     # try to implement a plain selection sort
+
+    # loop for all items in arr
+        # set min_index to current index
+        # loop from next index to the end of arr
+            # if current item is less than item at min_index
+                # set min_index to current index
+        # swap item at min_index with item at current index
     pass
