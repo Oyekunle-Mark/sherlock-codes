01c79e8514d77695b4b2e416895378f614173b0d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-02 11:44:34 +0100

    Implemented the pseudocode

--- minimum_swaps_second_pass.py
+++ minimum_swaps_second_pass.py
@@ -1,16 +1,27 @@
 def minimumSwaps(arr):
     # initialize indices to an empty dict
+    indices = {}
     # initialize swaps to zero
+    swaps = 0
     # loop through the arr and save the index of each value in indices
-    # loop through arr from one to length of arr plus one
+    for i, n in enumerate(arr):
+        indices[n] = i
+    # loop through arr to length of arr plus one
+    for i, n in enumerate(arr):
         # if current value is out of place
+        if arr[i] != i + 1:
             # save current value in temp
+            temp = arr[i]
             # get the index of the correct value from indices
+            index = indices[i + 1]
             # place correct value in the current index
+            arr[i] = i + 1
             # place current value in the index of from dict
-            # increment zero
+            arr[index] = temp
+            # increment swaps
+            swaps += 1
     # return swaps
-    pass
+    return swaps
 
 
 print(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))  # 5
