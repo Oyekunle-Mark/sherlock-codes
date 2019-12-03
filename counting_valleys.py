3865384ff72c0342e71b16604a2a108e22c87724
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-03 15:29:15 +0100

    Pseudocode for the first pass solution

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,2 +1,12 @@
 def countingValleys(n, s):
+    # initialize a variable level to zero
+    # initialize a variable valleys to zero
+    # loop through the steps in s
+        # if current step is an uphill one
+            # increment level by one
+        # if current step is a downhill one
+            # decrement level by one
+        # if level equals zero
+            # increment valleys by one
+    # return valleys
     pass
