f27c111c66b49b1d86b440c179008e7edc2d3c9c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-03 15:30:45 +0100

    Implemented a first pass

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,12 +1,21 @@
 def countingValleys(n, s):
     # initialize a variable level to zero
+    level = 0
     # initialize a variable valleys to zero
+    valleys = 0
     # loop through the steps in s
+    for step in s:
         # if current step is an uphill one
+        if step == 'U':
             # increment level by one
+            level += 1
         # if current step is a downhill one
+        else:
             # decrement level by one
+            level -= 1
         # if level equals zero
+        if level == 0:
             # increment valleys by one
+            valleys += 1
     # return valleys
-    pass
+    return valleys
