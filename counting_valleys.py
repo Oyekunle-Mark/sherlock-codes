d38f649852dc5bb6b737be394b23f553c01cbc75
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-03 15:42:54 +0100

    Formatted the code

--- counting_valleys.py
+++ counting_valleys.py
@@ -4,6 +4,7 @@ def countingValleys(n, s):
     # initialize a variable valleys to zero
     valleys = 0
     # loop through the steps in s
+
     for step in s:
         # if current step is an uphill one
         if step == 'U':
@@ -13,10 +14,12 @@ def countingValleys(n, s):
         elif step == 'D':
             # decrement level by one
             level -= 1
+
         # if level equals zero and last step was uphill
         if level == 0 and step == 'U':
             # increment valleys by one
             valleys += 1
+
     # return valleys
     return valleys
 
