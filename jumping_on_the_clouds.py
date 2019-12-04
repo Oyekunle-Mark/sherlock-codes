863ee27fcd462414279f634b5e75156bad2b876c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-04 17:33:56 +0100

    Implemented the how

--- jumping_on_the_clouds.py
+++ jumping_on_the_clouds.py
@@ -1,17 +1,24 @@
 def jumpingOnClouds(c):
     # should start jump at 0
     # initialize jump to zero
+    jump = 0
     # can only jump 1 or 2 index forward
     # initialize index to zero
+    index = 0
     # will run in O(n) time
     # while index is less than length of c
+    while index < len(c):
         # must avoid list items that are 0's
         # should jump 2 steps forward if there are two consecutive 1's
         # if the next item is 1 or the next two items are 0's
-            # increment jump by 1
+        if c[index + 1] == 1 or (c[index + 1] == 0 and c[index + 2] == 0):
             # increment index by 2
+            index += 2
         # otherwise
-            # increment jump by 1
+        else:
             # increment index by 1
+            index += 1
+        # increment jump by 1
+        jump += 1
     # return the number of jumps
-    pass
+    return jump
