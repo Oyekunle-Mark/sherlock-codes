2f2bd111730435b604cb4cb6c2044eae440676c5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-04 17:24:00 +0100

    The WHAT

--- jumping_on_the_clouds.py
+++ jumping_on_the_clouds.py
@@ -1,2 +1,8 @@
 def jumpingOnClouds(c):
+    # will run in O(n) time
+    # should start jump at 0
+    # can only jump 1 or 2 index forward
+    # must avoid list items that are 0's
+    # should jump 2 steps forward if there are two consecutive 1's
+    # return the number of jumps
     pass
