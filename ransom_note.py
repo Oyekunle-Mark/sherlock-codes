6d125e15d938552fba0bec3de7067ac66320bffe
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-05 09:59:55 +0100

    The what

--- ransom_note.py
+++ ransom_note.py
@@ -1,2 +1,8 @@
 def checkMagazine(magazine, note):
+    # checks if magazine contains the same words in note
+    # words are case sensitive
+    # magazine must contain the right count of words in magazine
+    # can be done in O(n) time
+    # count the words in note and magazine
+    # check to see that the words in note occur in the right number in magazine
     pass
