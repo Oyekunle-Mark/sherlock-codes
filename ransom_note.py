dac00ed35965b3a0ab9fbb6df6bd89d9db0ba682
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-05 10:14:45 +0100

    Added pseudocode how to implement the what

--- ransom_note.py
+++ ransom_note.py
@@ -5,4 +5,13 @@ def checkMagazine(magazine, note):
     # can be done in O(n) time
     # count the words in note and magazine
     # check to see that the words in note occur in the right number in magazine
+
+    mag_count = count(magazine)
+    note_count = count(note)
+
+    # loop through the key, value pair in note_count
+        # check if key is in mag_count
+            # check if value of key in mag_count is up to current value
+            # otherwise print no and break
+        # otherwise print no and break
     pass
