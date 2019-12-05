0ec8c35b98afe998730f513eeeadf7ac9ce9287a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-05 17:16:44 +0100

    The what

--- time_complexity_primality.py
+++ time_complexity_primality.py
@@ -1,2 +1,6 @@
 def primality(n):
+    # Finds if a number is prime or not
+    # Must check every number up to sqrt(n)
+    # Returns not prime as soon as a number is divisible by any number up to sqrt(n)
+    # Returns prime after testing every required number
     pass
