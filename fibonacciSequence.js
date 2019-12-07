dc4435a7f1a0b60cbab67ba1b213b09a2d39f2af
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 08:05:31 +0100

    The pseudocode

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -6,5 +6,18 @@
  */
 
 function fibonacci(n) {
-    
+    // finds the fibonacci sequence to up nth number
+    // Number ranges between 1 <= n <= 10
+
+    // initialize sequence to an empty array
+    // initialize count to zero
+    // if n is 1
+        // assign sequence to an array with a zero
+        // set count to one
+    // otherwise, if n is greater than or equal 2
+        // assign sequence to an array with zero and one
+        // set count to two
+    // loop while count of sequence is less than n
+        // append the sum of index n - 1 and n - 2 of sequence to sequence
+    // return sequence
 }
