66109160d3ca88fdb287facefc27a57b29afb8c0
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 07:56:29 +0100

    Fibonacci sequence skeleton

---
+++ fibonacciSequence.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the 'fibonacci' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function fibonacci(n) {
+    
+}
