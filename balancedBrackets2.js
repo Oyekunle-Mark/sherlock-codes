a91dab30cf4e8f907ef1c4abf9c70579d29aaf3c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 08:17:31 +0100

    Ban lanced bracket 2 skelton

---
+++ balancedBrackets2.js
@@ -0,0 +1,10 @@
+/*
+ * Complete the 'balancedBrackets' function below.
+ *
+ * The function is expected to return a BOOLEAN.
+ * The function accepts STRING string as parameter.
+ */
+
+function balancedBrackets(string) {
+
+}
