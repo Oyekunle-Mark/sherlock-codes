50c4bd228060269648543f571e7c8671b9bda825
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 08:58:58 +0100

    Second pass solution

--- balancedBrackets2.js
+++ balancedBrackets2.js
@@ -9,48 +9,30 @@ function balancedBrackets(string) {
   // should determine of the string contains matched bracket set or not
   // will have to loop through the whole string to check
   // will take O(n) time
-  // will use a stack based solution to check bracket pair
+  // The bracket set are "{}", "()", "[]" and "||"
 
-  const stack = [];
-  const mapBrackets = {
-    ')': '(',
-    ']': '[',
-    '}': '{',
-    '|': '|',
+  const brackets = [];
+  const bracketSet = {
+    ')': 1,
+    ']': 1,
+    '}': 1,
+    '|': 1,
+    '(': 1,
+    '[': 1,
+    '{': 1,
   };
 
   // loop through the string
   for (let i = 0; i < string.length; i++) {
-    // if current character in "([{"
-    if ('([{'.includes(string[i])) {
-      // push it to the stack
-      stack.push(string[i]);
-    }
-    // otherwise, if character in ")]}"
-    else if (')]}'.includes(string[i])) {
-      // pop an item from the stack
-      const item = stack.pop();
-      // if item popped is equal to mapBrackets of character
-      if (item === mapBrackets[string[i]]) {
-        // continue
-        continue;
-      }
-      // otherwise,
-      else {
-        // return false
-        return false;
-      }
-    }
+    // if current character in bracketSet
+        // add it to brackets
   }
-  // if stack is not empty
-  if (stack.length !== 0) {
-    // return false
-    return false;
-  }
-  // return true
-  return true;
+
+  return isMatched(brackets)
 }
 
 console.log(balancedBrackets('(([]))'));
 console.log(balancedBrackets('({[]})'));
 console.log(balancedBrackets('this(si{[]onw })'));
+console.log(balancedBrackets('{{||[]||}}'));
+console.log(balancedBrackets('[|]|'));
