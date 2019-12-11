832548d47edeae0627a58d10335cd7432f5dab6d
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-11 10:47:30 +0100

    The what

--- frequency_queries.py
+++ frequency_queries.py
@@ -1,2 +1,8 @@
 def freqQuery(queries):
+    # performs a set of operations based on input
+    # operations are insertion, deletion and frequency read
+    # use a dict to keep track of insertion and deletion
+    # use another dict to store frequency to make lookup O(n)
+    # store the result of frequency lookup in an array to be returned
+    # at the tail of the program
     pass
