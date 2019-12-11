a9aa7c6af53a7421feee964e693bdddbb6aa9f4e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-11 09:50:38 +0100

    The what

--- pairs.py
+++ pairs.py
@@ -1,2 +1,6 @@
 def pairs(k, arr):
+    # find the number of pairs of number having difference of k
+    # can run in O(n) time
+    # sorting will be required
+    # add k to the number and see if it exists
     pass
