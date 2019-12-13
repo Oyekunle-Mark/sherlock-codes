6aa991e69a9ae1af7c95fd5f28c375bc1e8b05c4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-13 12:20:07 +0100

    Refactored to make even simpler

--- shortestPath.py
+++ shortestPath.py
@@ -44,11 +44,8 @@ class Graph:
         self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
-            self.vertices[v2].add(v1)
-        else:
-            raise IndexError("That vertex does not exist!")
+        self.vertices[v1].add(v2)
+        self.vertices[v2].add(v1)
 
 
 def ShortestPath(strArr):
