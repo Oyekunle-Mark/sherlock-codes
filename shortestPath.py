e4ed07308688d10785e7c463162fe2ba79c87cd1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-13 12:18:56 +0100

    The graph adt

--- shortestPath.py
+++ shortestPath.py
@@ -34,6 +34,23 @@ class Queue():
         return len(self.queue)
 
 
+class Graph:
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+            self.vertices[v2].add(v1)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+
 def ShortestPath(strArr):
     # first, parse the input and represent the data like a graph
     # to find the shortest path, use bfs,
