d395d0a384c06ac7b24c35cad8d2e899d874b6c4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-13 12:17:28 +0100

    queue implementation

--- shortestPath.py
+++ shortestPath.py
@@ -13,6 +13,23 @@ Output: X-W
 """
 
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 def ShortestPath(strArr):
     # first, parse the input and represent the data like a graph
     # to find the shortest path, use bfs,
