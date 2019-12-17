3a444169d48569a8797af1469caba5072baeb097
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-17 09:58:58 +0100

    Sums the digit

--- recursive_digit_sum.py
+++ recursive_digit_sum.py
@@ -1,3 +1,7 @@
+def sumDigits(n):
+    pass
+
+
 def superDigit(n, k):
     # first creates a new string by concatenation n, k number of times
     # adds the digit in the result repeatedly until there is only one digit left
