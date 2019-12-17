a329ddb00eaeb27fb3ffe6a2cd74b1ceebb80d53
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-17 09:58:31 +0100

    The what

--- recursive_digit_sum.py
+++ recursive_digit_sum.py
@@ -1,2 +1,5 @@
 def superDigit(n, k):
+    # first creates a new string by concatenation n, k number of times
+    # adds the digit in the result repeatedly until there is only one digit left
+    # returns the final digit as an integer
     pass
