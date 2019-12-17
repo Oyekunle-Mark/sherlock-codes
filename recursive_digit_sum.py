9c19ea836c8b2af3ae614b16cfd186942f3e2ff2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-17 10:01:53 +0100

    Super digit logic

--- recursive_digit_sum.py
+++ recursive_digit_sum.py
@@ -6,4 +6,11 @@ def superDigit(n, k):
     # first creates a new string by concatenation n, k number of times
     # adds the digit in the result repeatedly until there is only one digit left
     # returns the final digit as an integer
+
+    # state a base case of if length of n is one
+        # return n as an int
+    # if k is not none
+        # assign the concatenation of n, k number of times to n
+    # call sumDigits on n and save the result in p
+    # return recursive call of superDigit with p
     pass
