e7270984692079fd1e21e007b1916e273a15a872
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-27 11:07:58 +0100

    used docstring for documentation'

--- frequency_queries.py
+++ frequency_queries.py
@@ -26,13 +26,14 @@ def updateFreq(d, prev, curr):
 
 
 def freqQuery(queries):
-    # performs a set of operations based on input
-    # operations are insertion, deletion and frequency read
-    # use a dict to keep track of insertion and deletion
-    # use another dict to store frequency to make lookup O(n)
-    # store the result of frequency lookup in an array to be returned
-    # at the tail of the program
-
+    """
+    performs a set of operations based on input
+    operations are insertion, deletion and frequency read
+    use a dict to keep track of insertion and deletion
+    use another dict to store frequency to make lookup O(n)
+    store the result of frequency lookup in an array to be returned
+    at the tail of the program
+    """
     # initialize op to empty dict
     op = {}
     # initialize freq to empty dict
