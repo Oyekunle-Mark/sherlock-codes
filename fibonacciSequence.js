9b5bd33c0bdd4db5e91b8477708bbb4677d5be40
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-27 11:12:25 +0100

    fib

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -1,5 +1,5 @@
 /*
- * Complete the 'fibonacci' function below.
+   Complete the 'fibonacci' function below.
  *
  * The function is expected to return an INTEGER_ARRAY.
  * The function accepts INTEGER n as parameter.
