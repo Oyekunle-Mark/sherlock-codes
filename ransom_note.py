48f6e6f42c924cb047759dd94727b01ba7ad11ab
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-27 11:14:58 +0100

    used docstring for documentation'

--- ransom_note.py
+++ ransom_note.py
@@ -11,13 +11,14 @@ def count(words):
 
 
 def checkMagazine(magazine, note):
-    # checks if magazine contains the same words in note
-    # words are case sensitive
-    # magazine must contain the right count of words in magazine
-    # can be done in O(n) time
-    # count the words in note and magazine
-    # check to see that the words in note occur in the right number in magazine
-
+    """
+    checks if magazine contains the same words in note
+    words are case sensitive
+    magazine must contain the right count of words in magazine
+    can be done in O(n) time
+    count the words in note and magazine
+    check to see that the words in note occur in the right number in magazine
+    """
     mag_count = count(magazine)
     note_count = count(note)
 
