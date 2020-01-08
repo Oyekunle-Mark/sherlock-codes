754c41404cb7ca562cc5c037a59fee1c816ba8b8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-08 08:24:59 +0100

    Docstring

--- kangaroo2.js
+++ kangaroo2.js
@@ -1,3 +1,11 @@
+/**
+ * Checks if a fixed increment of 1 converges with a fixed increment of 2
+ * @param {number} x1
+ * @param {number} v1
+ * @param {number} x2
+ * @param {number} v2
+ * @returns
+ */
 function kangaroo(x1, v1, x2, v2) {
   if ((x1 > x2 && v1 > v2) || (x2 > x1 && v2 > v1)) return 'NO';
 
