881c99741216a86869b2d2409818536199e6f6fc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-08 08:17:45 +0100

    Added test cases

--- sockMerchant2.js
+++ sockMerchant2.js
@@ -38,3 +38,6 @@ function sockMerchant(n, ar) {
 
   return pairCount;
 }
+
+console.log(sockMerchant(7, [1, 2, 1, 2, 1, 3, 2]));
+console.log(sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]));
