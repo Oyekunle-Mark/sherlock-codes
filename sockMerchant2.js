e17e48a0b3bc03af259c42cb8ec50237c5b697dd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-08 08:17:06 +0100

    Added comments

--- sockMerchant2.js
+++ sockMerchant2.js
@@ -6,27 +6,35 @@
  */
 function sockMerchant(n, ar) {
   /*
-  * This problem can be solved in linear time
-  * The first pass should count the occurrence of each number
-  * The second pass should check the number of pairs extractable from 
-  * number of each pari
-  * Return the overall pair count.
-  */
-  
-    const numOfEachSock = {};
-  let pairs = 0;
+   * This problem can be solved in linear time
+   * The first pass should count the occurrence of each number
+   * The second pass should check the number of pairs extractable from
+   * number of each pari
+   * Return the overall pair count.
+   */
 
+  // set sock count to an empty object
+  const countOfEachSock = {};
+  // set pair count zero
+  let pairCount = 0;
+
+  // loop and count each sock using countOfEachSock
   for (let i = 0; i < n; i++) {
-    if (!numOfEachSock[`${ar[i]}`]) numOfEachSock[`${ar[i]}`] = 0;
+    if (!countOfEachSock[ar[i]]) {
+      countOfEachSock[ar[i]] = 0;
+    }
 
-    numOfEachSock[`${ar[i]}`] += 1;
+    countOfEachSock[ar[i]] += 1;
   }
 
-  for (const key in numOfEachSock) {
-    const num = numOfEachSock[key];
+  // check the counts to see number of pairs obtainable
+  for (const key in countOfEachSock) {
+    const num = countOfEachSock[key];
 
-    if (num >= 2) pairs += Math.floor(num / 2);
+    if (num >= 2) {
+      pairCount += Math.floor(num / 2);
+    }
   }
 
-  return pairs;
+  return pairCount;
 }
