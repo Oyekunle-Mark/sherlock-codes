e57f0c3a1aef00100266e4f4f4cbea751c1bc403
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-08 07:59:46 +0100

    Improved readability

--- timeConversion.js
+++ timeConversion.js
@@ -26,25 +26,33 @@ console.log("TCL: timeConversion('02:34:50PM')", timeConversion('02:34:50PM'));
 
 function timeConversion2(s) {
   /*
-   * Write your code here.
+   * The only times when some conversion needs to take place are
+   * when it is after 1pm or when it is around 12am
+   * Every other time should be returned without the period
    */
   // grab a copy of the input 12-hour time
   const time = s;
+  // grab the time period
+  const period = time.slice(8);
+  // store the hour
+  const hour = time.slice(0, 2);
+  // keep the minutes and seconds
+  const minutesAndSeconds = time.slice(2,8);
 
   // if the time is in PM and not the twelfth hour
-  if (time[8] === 'P' && time.slice(0, 2) !== '12') {
+  if (period === 'PM' && hour !== '12') {
     // return the 24-hour format by adding twelve and stripping the PM
-    return Number(time.slice(0, 2)) + 12 + time.slice(2, 8);
+    return Number(hour) + 12 + minutesAndSeconds;
   }
 
   // if time is in AM and it is the twelfth hour
-  if (time[8] === 'A' && time.slice(0, 2) === '12') {
+  if (period === 'AM' && hour === '12') {
     // set the hour to 00
-    return '00' + time.slice(2, 8);
+    return '00' + minutesAndSeconds;
   }
 
-  // otherwise, strip the AM 
-  return time.slice(0, 8);
+  // otherwise, strip the AM
+  return hour + minutesAndSeconds;
 }
 
 console.log(
