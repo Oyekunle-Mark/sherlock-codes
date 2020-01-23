2399641a8c61a6ed3ba719a5955873aa6519be6a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-23 14:32:19 +0100

    Fleshed out the logic

--- queue_to_do.py
+++ queue_to_do.py
@@ -7,16 +7,28 @@ def solution(start, length):
 
     # HOW!
     # initialize ids to empty list
+    ids = []
     # initialize start to one
+    start = 1
     # initialize max to the square of length
+    max = length * length
     # initialize offset to length
+    offset = length
     # initialize cutoff to zero
+    cutoff = 0
     # loop while start is less than max
+    while start < max:
         # if start modulo length is equivalent to cutoff
+        if start % length == cutoff:
             # add start to ids
+            ids.append(start)
             # add cutoff to start
+            start += cutoff
             # increment cutoff
-        # otherwise, 
+            cutoff += 1
+        # otherwise,
+        else:
             # add start to ids
+            ids.append(start)
     # return ids
-    pass
+    return ids
