b33c3de4e3f868ce607fa8d6db86d01de8fe4c0f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-08 18:36:34 +0100

    Solution from the leetcode editor

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -10,3 +10,39 @@ Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
 """
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # define two strings int_str1 and int_str2 to hold the int from both linkedlist
+        int_str1 = int_str2 = ''
+        # extract the numbers from l1 and add to int_str1
+        curr = l1
+        while curr is not None:
+            int_str1 = str(curr.val) + int_str1
+            curr = curr.next
+        # extract the numbers from l2 and add to int_str2
+        curr = l2
+        while curr is not None:
+            int_str2 = str(curr.val) + int_str2
+            curr = curr.next
+        # cast both strings to integers
+        # add the values
+        result = str(int(int_str1) + int(int_str2))
+        # convert to linked list and return the result
+        ret = None
+        len_result = len(result) - 1
+        for i in range(len_result, -1, -1):
+            if i == len_result:
+                ret = ListNode(result[i])
+                curr = ret
+            else:
+                curr.next = ListNode(result[i])
+                curr = curr.next
+        # return the linked list
+        return ret
