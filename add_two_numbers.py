3a799a19282173457f8a7dda05151731397bae26
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-08 18:37:12 +0100

    Uncommented the  listnode class

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -11,10 +11,12 @@ Explanation: 342 + 465 = 807.
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
