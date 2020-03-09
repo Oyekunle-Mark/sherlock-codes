2621311fafaaadb2820f34f39a905d232e6b06ba
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-03-09 16:15:51 +0100

    First pass implementation of the sliding window approach

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -54,20 +54,36 @@ class Solution:
         # SLIDING WINDOW APPROACH
         # set count and max_count to zero
         # set i and j to zero
+        count = max_count = i = j = 0
         # set char_set to an empty set
+        char_set = set()
         # find the length of s and set to len_s
+        len_s = len(s)
         # loop while i and j are less than len_s
+        while(i < len_s and j < len_s):
             # if character at index j of s is in char_set
+            if s[j] in char_set:
                 # increment i by one and set j to i
+                i += 1
+                j = 1
                 # set char_set to an empty set
+                char_set = set()
                 # set count to zero
+                count = 0
             # otherwise
+            else:
                 # increment count
+                count += 1
                 # if count is greater than max_count
+                if count > max_count:
                     # set max_count to count
+                    max_count = count
                 # add the current character to the char_set
+                char_set.add(s[j])
                 # increment j
+                j += 1
         # return max_count
+        return max_count
 
 
 s = Solution()
